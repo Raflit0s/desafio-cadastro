7647cd7d6ae35292bd2f8a06df30ec5bf8f9f67e
Raflit0s <rafinhaprincipe36@gmail.com>
2025-04-07 09:57:28 -0300

    User input in menu and InputMismatchException handling implemented

--- desafioCadastro/src/application/Main.java
+++ desafioCadastro/src/application/Main.java
@@ -5,7 +5,12 @@ import util.Menu;
 
 public class Main {
     public static void main(String[] args) {
+        Form form = new Form();
         Menu menu = new Menu();
+
+        form.createForm();
+        form.readForm();
+        System.out.println();
         menu.initialMenu();
         }
     }
--- desafioCadastro/src/util/Menu.java
+++ desafioCadastro/src/util/Menu.java
@@ -1,15 +1,27 @@
 package util;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Menu {
 
     public void initialMenu() {
-             StringBuilder sb = new StringBuilder("1. Cadastrar um novo pet\n")
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder("1. Cadastrar um novo pet\n")
                 .append("2. Alterar os dados do pet cadastrado\n")
                 .append("3. Deletar um pet cadastrado\n")
                 .append("4. Listar todos os pets cadastrados\n")
                 .append("5. Listar pets por algum critério (idade, nome, raça)\n")
                 .append("6. Sair");
 
-             System.out.println(sb);
+        int input = 0;
+        try {
+            do {
+                System.out.println(sb);
+                input = sc.nextInt();
+            } while (input != 6);
+        } catch(InputMismatchException e) {
+        System.out.println("Apenas números inteiros são válidos");
+    }
     }
 }
